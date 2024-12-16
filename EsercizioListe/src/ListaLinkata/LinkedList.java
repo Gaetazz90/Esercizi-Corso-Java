fbffd3eade85293c5403138ee9f3c21465ef977f
Gaetazz90 <gaetano.lataza@gmail.com>
2024-12-16 22:57:41 +0100

    Generics

--- EsercizioListe/src/ListaLinkata/LinkedList.java
+++ EsercizioListe/src/ListaLinkata/LinkedList.java
@@ -1,6 +1,35 @@
 package ListaLinkata;
 
+
 public class LinkedList<T> {
+
+    private static class Elemento <T> {
+        T valore;
+        Elemento<T> prossimo;
+
+        public Elemento(T valore) {
+            this.valore = valore;
+            this.prossimo = null;
+        }
+    }
+
+    Elemento<T> testa;
+
+    public void addFirst(T valore){
+        Elemento<T> nuovoElemento = new Elemento<>(valore);
+        nuovoElemento.prossimo = testa;
+        testa = nuovoElemento;
+    }
+
+    public T removeFirst(){
+        if(testa == null){
+            System.out.println("Coda vuota, hai rimosso già tutto");
+            return null;
+        }
+        T vecchiaTesta = testa.valore;
+        testa = testa.prossimo;
+        return vecchiaTesta;
+    }
 
 }
 
--- EsercizioListe/src/PilaDiPiatti/PilaDiPiatti.java
+++ EsercizioListe/src/PilaDiPiatti/PilaDiPiatti.java
@@ -1,5 +1,45 @@
 package PilaDiPiatti;
 
+
 public class PilaDiPiatti<T> {
+
+    private static class Elemento <T> {
+        T valore;
+        Elemento<T> precedente;
+
+        public Elemento(T valore) {
+            this.valore = valore;
+            this.precedente = null;
+        }
+
+    }
+
+    Elemento<T> cima;
+
+
+    public void push(T valore){
+        Elemento<T> nuovoElemento = new Elemento<T>(valore);
+        nuovoElemento.precedente = cima;
+        cima = nuovoElemento;
+    }
+
+    public T peek () {
+        if(cima == null){
+            System.out.println("Pila vuota, hai rimosso già tutto");
+            return null;
+        }
+        T valoreCima = cima.valore;
+        return valoreCima;
+    }
+
+    public T pop () {
+        if(cima == null){
+            System.out.println("Pila vuota, hai rimosso già tutto");
+            return null;
+        }
+        T vecchiaCima = cima.valore;
+        cima = cima.precedente;
+        return vecchiaCima;
+    }
     
 }
